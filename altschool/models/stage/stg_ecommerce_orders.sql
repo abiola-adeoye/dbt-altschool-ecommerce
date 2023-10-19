4338f9073832c4527f050262d8cfc00ad272d035
abiola-adeoye <slmadeoye@gmail.com>
2023-10-19 16:29:51 +0100

    first commit

---
+++ altschool/models/stage/stg_ecommerce_orders.sql
@@ -0,0 +1,24 @@
+with source as (
+
+    select * from {{ source('thelook_ecommerce', 'orders') }}
+
+)
+
+select
+    -- ids
+    order_id,
+    user_id,
+
+
+    -- timestamp
+    gender,
+    created_at,
+    returned_at,
+    shipped_at,
+    delivered_at,
+
+    -- other columns
+    status,
+    num_of_item
+
+from source
